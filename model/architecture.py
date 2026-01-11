3a921e07ae0e38ddb6e2cc704609c8fca9e8449c
Tygo <tcrawley@uwo.ca>
2026-01-10 20:07:59 -0500

    generating text

--- model/architecture.py
+++ model/architecture.py
@@ -177,3 +177,17 @@ class GPTModelFlashAttn(nn.Module):
         logits = self.out_head(x)
 
         return logits
+
+
+def generate_text_simple(model, idx, max_new_tokens, context_size):
+    for _ in range(max_new_tokens):
+        idx_cond = idx[:, -context_size:]
+        with torch.no_grad():
+            logits = model(idx_cond)
+
+        logits = logits[:, -1, :]
+        probas = torch.softmax(logits, dim=-1)
+        idx_next = torch.argmax(probas, dim=-1, keepdim=True)
+        idx = torch.concat((idx, idx_next), dim=1)
+    
+    return idx
